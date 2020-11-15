9f6c85c36a647b515e7cb13aea8dfd4476398cc9
volodymyrb <volodymyrb@fornova.net>
2020-11-15 22:17:51 +0200

    get subject route

--- schedule/crud.py
+++ schedule/crud.py
@@ -11,3 +11,8 @@ async def create_subject(subject_data: shemes.Subject) -> models.Subject:
     subject_dict = subject_data.dict()
     subject: models.Subject = await models.Subject.create(**subject_dict)
     return subject
+
+
+async def get_subject(name: str) -> models.Subject:
+    subject = await models.Subject.get(name=name)
+    return subject
