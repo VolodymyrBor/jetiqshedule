1c3e79aa5051267334d4a8cf53c6a38fbe61ae9e
volodymyrb <volodymyrb@fornova.net>
2020-11-15 21:56:54 +0200

    updated routers

--- schedule/route.py
+++ schedule/route.py
@@ -9,11 +9,11 @@ from .shemes import Subject
 schedule = APIRouter()
 
 
-@schedule.get('/', response_model=List[Subject], tags=['subjects'])
+@schedule.get('/subject/', response_model=List[Subject], tags=['subjects'])
 async def get_all_subjects():
     return await crud.get_all_subjects()
 
 
-@schedule.put('/', response_model=Subject, tags=['subjects'], status_code=status.HTTP_201_CREATED)
+@schedule.put('/subject/', response_model=Subject, tags=['subjects'], status_code=status.HTTP_201_CREATED)
 async def create_subject(subject: Subject):
     return await crud.create_subject(subject)
